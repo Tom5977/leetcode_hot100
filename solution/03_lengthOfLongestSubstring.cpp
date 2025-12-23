e81ea7b000ff06bcaaf0563d9b43527d044fc433
Tom5977 <83262661+Tom5977@users.noreply.github.com>
2025-12-23 14:01:36 +0800

    03

---
+++ solution/03_lengthOfLongestSubstring.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <map>
+using namespace std;
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        map<char, bool>m;
+        int ans = 0;
+        for (auto c : s) {
+            if (m[c]) {
+                if (m.size() > ans) ans = m.size();
+                m.clear();
+            }
+                m[c] = true;
+
+        }
+        return ans;
+    }
+};
